f12f823bea0a64076dd8036ead2e4bb90ce687ec
Moises Carvalho <moises@icomp.ufam.edu.br>
2025-05-16 19:51:44 -0400

    Aula do Numero Primo

--- algPrimos/primo.go
+++ algPrimos/primo.go
@@ -1,50 +1,80 @@
 package main
   
-import "fmt"
+import (
+    "errors"
+    "fmt"
+)
   
-func echo(name string) string {
-    return name
-}
  
-func soma(num1 int, num2 int) int {
+func ehPrimo(numero int) bool {
 	var cont int
-	var acum int
-	
-	acum = 0
-	cont = 0
-	for(cont < num1){
-		acum = acum + num2
-		//fmt.Println(acum)
-		cont = cont + 1
+	var ePrimo bool
+	ePrimo = true
+	for cont = 2; cont < numero; cont++ {
+		if (numero % cont)  == 0 {
+			fmt.Println(cont)
+			ePrimo = false
+		}
 	}
-	return acum
-} 
-  
-func expon(base int, exponte int) int {
+	return ePrimo
+}   
+
+func ehPrimoV2(numero int) (bool, error) {
 	var cont int
-	var acum int
+	var ePrimo bool
+	var primoNeg error
 	
-	acum = 1
-	cont = 0
-	for(cont < exponte){
-		acum = soma(acum,base)
-		//fmt.Println(acum)
-		cont = cont + 1
+	primoNeg = nil
+	ePrimo = false
+	if (numero > 0) {
+		if (numero >= 2) {
+			ePrimo = true
+			cont = 2
+			for  (cont < numero) && ePrimo {
+				if (numero % cont)  == 0 {
+					ePrimo = false
+				}
+				cont++
+			}
+		}
+	} else {
+		primoNeg = errors.New("Nao se calcula primo negativo")
 	}
-	return acum
+	return ePrimo, primoNeg
 }   
     
-func  main(){
+func main(){
   	   
-	   var numero1 int
-	   var numero2 int
-	   var numero3 int
+	  // var numero1 int
+	  // var numero2 int
+	  // var numero3 int
+	  var cont int
 	   
-           numero1 = 3
-           numero2 = 5
-           numero3 = expon(numero1,numero2)
-           fmt.Println(numero3)
-           
-           
-           
-  }
+         //  numero1 = 3
+         //  numero2 = 5
+          
+          // fmt.Println(ehPrimo(1000002))
+          
+           var numerosTeste []int
+           numerosTeste = make([]int, 7)
+           numerosTeste[0] = -1
+           numerosTeste[1] = 0
+           numerosTeste[2] = 1
+           numerosTeste[3] = 2
+           numerosTeste[4] = 5
+           numerosTeste[5] = 12
+           numerosTeste[6] = 37
+          
+           for cont = 0; cont < 7; cont++ {
+            	fmt.Println(ehPrimoV2(numerosTeste[cont]))
+	   }
+          
+          
+         /*  fmt.Println(ehPrimoV2(-1))   
+           fmt.Println(ehPrimoV2(0))
+           fmt.Println(ehPrimoV2(1))
+           fmt.Println(ehPrimoV2(2))
+ 	   fmt.Println(ehPrimoV2(5))
+ 	   fmt.Println(ehPrimoV2(12))
+ 	   fmt.Println(ehPrimoV2(37)) */
+}
